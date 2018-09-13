a5b5f8f40bee20e30fb2323f38195ea455d4f747
Ashish Malhotra <ash6320@gmail.com>
2018-09-12 23:09:46 -0700

    Add implementation of PID controller. Car drives around test track with this controller.

--- src/PID.h
+++ src/PID.h
@@ -9,10 +9,12 @@ public:
   double p_error;
   double i_error;
   double d_error;
+  double prev_cte;
+  double integ_cte;
 
   /*
   * Coefficients
-  */ 
+  */
   double Kp;
   double Ki;
   double Kd;
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,9 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  // Initial value of params chosen manually through trial and error
+  // Very small Ki is used since the vehicle doesnt have a steering bias.
+  pid.Init(0.15, 0.001, 2.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,7 +60,8 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+          pid.UpdateError(cte);
+          steer_value = -pid.TotalError();
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
